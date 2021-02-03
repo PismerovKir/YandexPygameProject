363b38cb372eadef308868a16223ce68073ec909
ilnxnsts <ilyina.a.nastya@gmail.com>
2021-02-03 14:39:24 +0300

    теперь есть ракета, которая может двигаться. Осталось сделать анимацию ее огоньков и добавить фон с врагами, это я постараюсь сделать завтра

--- Тест прога.py
+++ Тест прога.py
@@ -1,5 +1,58 @@
-print('Здесь будет разработка проги, а потом перенос в ветку мастер')
-print('Чек разработку')
-print('Орну если '
-      'то пичарм тупит')
-print(4)
+import pygame
+from os import path
+
+WIDTH = 1000
+HEIGHT = 800
+FPS = 100
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+
+
+class SpaceShip(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((80, 90))
+        self.image = pygame.image.load(path.join("korabl1.png")).convert()
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 20
+        self.speedX = 0
+
+    def update(self):
+        self.speedX = 0
+        k = pygame.key.get_pressed()
+        if k[pygame.K_LEFT]:
+            self.speedX = -8
+        if k[pygame.K_RIGHT]:
+            self.speedX = 8
+        self.rect.x += self.speedX
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 10:
+            self.rect.left = 0
+
+
+
+all_sprites = pygame.sprite.Group()
+spaceship = SpaceShip()
+all_sprites.add(spaceship)
+
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    all_sprites.update()
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+pygame.quit()
